4e9f86dbe5a5b3c8ee45209f8ce4ea0078147faf
Boris Ding <ding@pleisto.com>
2026-06-13 01:11:41 +0800

    fix: remove unused import of RiSearchLine from web-tools.tsx

--- app/webui/src/apps/console/sections/web-tools.tsx
+++ app/webui/src/apps/console/sections/web-tools.tsx
@@ -1,4 +1,4 @@
-import { RiSaveLine, RiSearchLine } from '@remixicon/react'
+import { RiSaveLine } from '@remixicon/react'
 import { useMutation, useQuery, useQueryClient } from '@tanstack/react-query'
 import { useEffect, useMemo, useState } from 'react'
 import { useTranslation } from 'react-i18next'
